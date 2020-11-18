ba76d22beff21315af6f7f5db3c0cfdab11b2e02
corogit <n.ohta5192@gmail.com>
2020-11-18 12:07:05 +0000

    3rd commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-  
-   
-    
+
+
+
   def edit
     @user = User.find(params[:id])
   end
@@ -21,10 +21,12 @@ class UsersController < ApplicationController
   def index
      @user = User.new
      @users = User.all
+     @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
     @books = @user.books
   end
 
