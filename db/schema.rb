bd029a3b9c3242acb41d7e9572fd94263bc8b929
corogit <n.ohta5192@gmail.com>
2020-11-21 10:27:42 +0000

    9th commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_043147) do
+ActiveRecord::Schema.define(version: 2020_11_21_080632) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_043147) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "profile_image_id"
   end
 
   create_table "users", force: :cascade do |t|
