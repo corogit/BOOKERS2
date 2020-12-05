8d84bb05f18a2c9c7da9adbe4dcb30161640086a
corogit <n.ohta5192@gmail.com>
2020-12-05 05:50:13 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
     end
     end
 
-    resources :relationships, only: [:create, :destroy, :followings, :followers]
+    resources :relationships, only: [:create, :destroy]
 end
